e9906a06ca416b2096f866d41e989a71cb5b3b5c
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-30 18:31:59 +0500

    languageWebApp

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      screens: {
+        small: "450px",
+        tablet: "768px",
+        desktop: "1024px",
+        large: "1280px",
+        xlarge: "1536px",
+      },
+      colors: {
+      primary: "#00555A",
+      secondary: "#BBA04E",
+      background: "#F9FAFB",
+      text: "#ffffff",
+      secondaryText:'#000000',
+      border: "#DDDDDD",
+      },
+    },
+  },
+  plugins: [],
+}
+
